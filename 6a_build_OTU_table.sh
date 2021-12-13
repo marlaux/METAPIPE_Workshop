2960750aeb79835f493552e1a9f7330676c2331f
Marcele Laux <marcelelaux@gmail.com>
2021-12-13 14:35:45 -0300

    Add files via upload

---
+++ 6a_build_OTU_table.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT="6a_OTU_contingency_table.py"
+REPRESENTATIVES=$1
+#filename=$(basename -- ${REPRESENTATIVES})
+#filename="${filename%.*}"
+QUALITY=$2
+STATS="${REPRESENTATIVES/_representatives.fas/.stats}"
+SWARMS="${REPRESENTATIVES/_representatives.fas/.swarms}"
+UCHIME="${REPRESENTATIVES/.fas/.uchime}"
+ASSIGNMENTS=$3
+OTU_TABLE="${REPRESENTATIVES/_representatives.fas/_OTU_table_complete.tab}"
+PWD="${4}"
+
+#module --force purge
+#module load StdEnv
+#module load Python/2.7.15-GCCcore-8.2.0
+#source ~/anaconda3/etc/profile.d/conda.sh
+#source ~/.bashrc
+#conda init bash
+#conda activate env_py27
+conda run -n py27 python \
+    "${SCRIPT}" \
+    "${REPRESENTATIVES}" \
+    "${STATS}" \
+    "${SWARMS}" \
+    "${UCHIME}" \
+    "${QUALITY}" \
+    "${ASSIGNMENTS}" \
+    ${PWD}/*_dp.fasta > "${OTU_TABLE}" 2>/dev/null
+
+
+
---
+++ DB1_NCBI_download_refs.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+TAXON_NAME=''
+TXID=''
+MARKER=''
+
+usage () {
+        echo "##################################################"
+        echo " "
+        echo "DOWNLOAD NCBI SEQUENCES"
+        echo " "
+        echo "Usage: ${0} [-n taxon name] [-t taxonid] [-m marker]"
+        echo "-n     target taxon name, e.g. Arachnida"
+        echo "-t     NCBI Taxonomy ID, e.g txid6854"
+        echo "-m     marker, e.g. COI, ITS, ITS1, ITS2, trnL, rbcL, matK..."
+        echo "-h     print this help"
+        echo " "
+        echo "##################################################"
+                > /dev/null 2>&1; exit 1;
+
+}
+while getopts "n:t:m:h" option; do
+        case $option in
+        n) TAXON_NAME="${OPTARG}"
+                ;;
+        t) TXID="${OPTARG}"
+                ;;
+        m) MARKER="${OPTARG}"
+                ;;
+        h | *) usage
+                exit 0
+                ;;
+        \?) echo "Invalid option: -$OPTARG"
+                exit 1
+                ;;
+   esac
+done
+
+if [ -z "${TAXON_NAME}" ] || [ -z "${TXID}" ] || [ -z "${MARKER}" ]
+        then
+                echo 'Missing argument' >&2
+                exit 1
+fi
+if [ "$MARKER" = "COI" ] || [ "$MARKER" = "CO1" ] || [ "$MARKER" = "COX" ]; then
+        QUERY="${TXID} [ORGN] AND (COI [TI] OR CO1 [TI] OR COX [TI] OR cytochrome oxidase subunit 1 [TI])"
+        OUT="${TAXON_NAME}_${MARKER}_NCBI.fasta"
+        esearch -db nuccore -query "${QUERY}" | efetch -format fasta >> "${OUT}"
+fi
+
+if [ "$MARKER" = "ITS1" ] || [ "$MARKER" = "ITS-1" ] || [ "$MARKER" = "ITS 1" ]; then
+        QUERY="${TXID} [ORGN] AND (ITS1 [TI] OR ITS-1 [TI] OR ITS 1 [TI] OR ITS_1 [TI] OR internal transcribed spacer 1 [TI])"
+        OUT="${TAXON_NAME}_${MARKER}_NCBI.fasta"
+        esearch -db nuccore -query "${QUERY}" | efetch -format fasta >> "${OUT}"
+fi
+
+if [ "$MARKER" = "ITS2" ] || [ "$MARKER" = "ITS-2" ] || [ "$MARKER" = "ITS 2" ]; then
+        QUERY="${TXID} [ORGN] AND (ITS2 [TI] OR ITS-2 [TI] OR ITS 2 [TI] OR ITS_2 [TI] OR internal transcribed spacer 2 [TI])"
+        OUT="${TAXON_NAME}_${MARKER}_NCBI.fasta"
+        esearch -db nuccore -query "${QUERY}" | efetch -format fasta >> "${OUT}"
+fi
+
+if [ "$MARKER" = "ITS" ]; then
+        QUERY="${TXID} [ORGN] AND (ITS1 [TI] OR ITS2 [TI] OR internal transcribed spacer [TI])"
+        OUT="${TAXON_NAME}_${MARKER}_NCBI.fasta"
+        esearch -db nuccore -query "${QUERY}" | efetch -format fasta >> "${OUT}"
+fi
+
+if [ "$MARKER" != "COI" ] || [ "$MARKER" != "CO1" ] || [ "$MARKER" != "COX" ] || [ "$MARKER" != "ITS" ] || [ "$MARKER" != "ITS1" ] || [ "$MARKER" != "ITS-1" ] || [ "$MARKER" != "ITS 1" ] || [ "$MARKER" != "ITS2" ] || [ "$MARKER" != "ITS-2" ] || [ "$MARKER" != "ITS 2" ]; then
+        OUT="${TAXON_NAME}_${MARKER}_NCBI.fasta"
+        esearch -db nuccore -query "${TXID} [ORGN] AND ${MARKER} [TI]" | efetch -format fasta >> "${OUT}"
+fi
